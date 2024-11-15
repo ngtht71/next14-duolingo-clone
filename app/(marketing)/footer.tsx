da0cc957ac3f6c8252291d5b3d14d4e2efa4e16b
ngtht71 <22028252@vnu.edu.vn>
2024-11-15 22:45:35 +0700

    change language to vietnamese

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+      {/* <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
         <Button size="lg" variant="ghost" className="w-full">
           <Image 
             src="/hr.svg" 
@@ -55,7 +55,7 @@ export const Footer = () => {
           />
           Japanese
         </Button>
-      </div>
+      </div> */}
     </footer>
   );
 };
--- components/quests.tsx
+++ components/quests.tsx
@@ -14,14 +14,14 @@ export const Quests = ({ points }: Props) => {
     <div className="border-2 rounded-xl p-4 space-y-4">
       <div className="flex items-center justify-between w-full space-y-2">
         <h3 className="font-bold text-lg">
-          Quests
+          Nhiệm vụ
         </h3>
         <Link href="/quests">
           <Button
             size="sm"
             variant="primaryOutline"
           >
-            View all
+            Xem tất cả
           </Button>
         </Link>
       </div>
